e0f38be74dc31fb7e4c3e3a62ff92d6bc64fa38e
lubo <173774418@qq.com>
2021-08-11 15:12:37 +0800

    添加配置文件

---
+++ index.config.ts
@@ -0,0 +1,28 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-03 14:31:05
+ * @LastEditTime: 2021-08-11 10:52:52
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \fline-meeting-manage-wechat\src\pages\index\index.config.ts
+ */
+export default {
+	navigationBarTitleText: '会议预约',
+	backgroundTextStyle: 'light',
+	backgroundColor:'#1E6FFF',
+	navigationBarBackgroundColor: '#1E6FFF',
+	navigationBarTextStyle: 'white',
+	usingComponents: {
+		"van-datetime-picker": "@vant/datetime-picker/index",
+		"van-popup": "@vant/popup/index",
+		"van-button": "@vant/button/index",
+		"van-tabbar": "@vant/tabbar/index",
+		"van-tabbar-item": "@vant/tabbar-item/index",
+		"van-icon": "@vant/icon/index",
+		"van-field": "@vant/field/index",
+		"van-cell-group": "@vant/cell-group/index",
+		"van-row": "@vant/row/index",
+		"van-col": "@vant/col/index",
+		"van-toast": "@vant/toast/index",
+	}
+}
